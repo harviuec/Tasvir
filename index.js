7df1cfd41e30520c0db463a7c5111a2bbdd43c47
Harvinder Singh <hs3499@nyu.edu>
2018-10-13 18:58:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const Jimp = require('jimp');
+const path = require('path');
+const multer = require('multer')();
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+
+app.post('/resize', multer.single('image'), (req, res) => {
+  const file = req.file;
+  const fileBuffer = file && file.buffer;
+  Jimp.read(fileBuffer).then(image => {
+    const height = image.getHeight();
+    const width = image.getWidth();
+    const imageMime = image.getMIME();
+    let newDimension = height;
+    if (height > width) {
+      newDimension = width;
+    }
+    image.resize(newDimension, newDimension);
+    image.getBase64Async(imageMime).then((buffer) => {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<img src="');
+      res.write(buffer);
+      res.end('" />');
+    });
+  }).catch(err => {
+    res.send(err);
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
