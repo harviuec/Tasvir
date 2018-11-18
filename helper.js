be8b18494160015e9bbb921f2ef7c354f4052c6d
Harvinder Singh <hs3499@nyu.edu>
2018-11-17 19:53:07 -0500

    third assignment

--- helper.js
+++ helper.js
@@ -1,7 +1,8 @@
+const bcrypt = require('bcrypt');
+const xss = require('xss');
+const fs = require('fs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
-
 const saltRounds = 10;
 
 function createHash(password) {
@@ -33,17 +34,50 @@ const createUser = (userData) => {
 const checkUser = (userData) => {
   const {userName, password} = userData;
   return User.findOne({userName}).then(user => {
+    if (!user) {
+      throw new Error('user does not exist');
+    }
     const hash = user.password;
     return verifyHash(password, hash).then(result => {
       if (result === false){
         throw new Error('password does not match');
       }
+      return user;
     })
-  })
+  });
+};
+
+const verifyDetails = ({userName, password}) => {
+  userName = xss(userName);
+  password = xss(password);
+  return {userName, password};
+};
+
+const connectDb = () => {
+  mongoose.connect('mongodb://localhost:27017/tasvir', {useNewUrlParser: true});
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function callback() {
+    console.log("Database connected");
+  });
+};
+
+const checkImageValidity = (image) => {
+  const height = image.getHeight();
+  const width = image.getWidth();
+  const imageMime = image.getMIME().split('/');
+  const allowedTypes = ['jpeg', 'jpg', 'png'];
+  if (!width || !height || imageMime[0] !== 'image' || allowedTypes.indexOf(imageMime[1]) === -1) {
+    return false;
+  }
+  return true;
 };
 
 module.exports = {
   createUser,
-  checkUser
+  checkUser,
+  verifyDetails,
+  connectDb,
+  checkImageValidity
 };
 
--- index.js
+++ index.js
@@ -5,33 +5,118 @@ const multer = require('multer')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helpers = require('./helper');
-const xss = require('xss');
-const app = express();
+const uuid = require('uuid/v4');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const morgan = require('morgan');
+const fs = require('fs');
 const port = 3000;
+
+
+// passport configuration
+passport.use(new LocalStrategy(
+  {usernameField: 'userName'},
+  (userName, password, done) => {
+    helpers.checkUser({userName, password}).then(user => {
+      return done(null, user);
+    }).catch(err => {
+      return done(err);
+    });
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.userName);
+});
+
+passport.deserializeUser((userName, done) => {
+  done(null, {userName});
+});
+
+
+// express middlewares
+const app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' });
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use(session({
+  genid: (req) => {
+    return uuid();
+  },
+  store: new MongoStore({mongooseConnection: mongoose.connection}),
+  secret: 'ibn batuta',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-function connectDb() {
-  mongoose.connect('mongodb://localhost/tasvir');
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function callback() {
-    console.log("Database connected");
-  });
-}
 
-function verifyDetails({userName, password}) {
-  userName = xss(userName);
-  password = xss(password);
-  return {userName, password};
-}
+// app routes
+app.get('/images/:image', (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const imageName = req.params && req.params.image;
+      const userName = req.user && req.user.userName;
+      const filePath = `./uploads/${userName}/${imageName}`;
+      fs.readFile(filePath, function (err, data) {
+        if (err) {
+          throw err;
+        }
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.end(data);
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+app.get('/logout', (req, res) => {
+  try {
+    req.logout();
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(401);
+  }
+});
+
+app.get('/check', (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const userName = req.user && req.user.userName;
+      const filePath = `./uploads/${userName}/`;
+      fs.readdir(filePath, function (err, items) {
+        items = items || [];
+        const allImages = [];
+        for (let i = 0; i < items.length; i++) {
+          allImages.push(`/images/${items[i]}`);
+        }
+        res.send({images: allImages});
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 
 app.post('/signup', (req, res) => {
   try {
-    const userDetails = verifyDetails(req.body);
+    const userDetails = helpers.verifyDetails(req.body);
     helpers.createUser(userDetails).then(data => {
       res.sendStatus(200);
     }).catch(err => {
@@ -43,51 +128,62 @@ app.post('/signup', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const userDetails = verifyDetails(req.body);
-  helpers.checkUser(userDetails).then(data => {
-    res.sendStatus(200);
-  }).catch(err => {
-    res.sendStatus(500);
-  });
+  passport.authenticate('local', (err, user) => {
+    req.login(user, (err) => {
+      if (err) {
+        return res.sendStatus(401);
+      }
+      try {
+        const userName = user && user.userName;
+        const filePath = `./uploads/${userName}/`;
+        fs.readdir(filePath, function (err, items) {
+          items = items || [];
+          const allImages = [];
+          for (let i = 0; i < items.length; i++) {
+            allImages.push(`/images/${items[i]}`);
+          }
+          res.send({images: allImages});
+        });
+      } catch (e) {
+        res.sendStatus(500);
+      }
+    });
+  })(req, res);
 });
 
 app.post('/resize', multer.single('image'), (req, res) => {
-  const userName = req.body.userName;
-  const password = req.body.password;
-  helpers.checkUser({userName, password}).then(() => {
-    const size = req.body.newSize;
-    const file = req.file;
-    const fileBuffer = file && file.buffer;
-    Jimp.read(fileBuffer).then(image => {
-      const height = image.getHeight();
-      const width = image.getWidth();
-      if (!width || !height) {
-        throw new Error('file not recognized');
-      }
-      const imageMime = image.getMIME();
-      let newDimension = height;
-      if (height > width) {
-        newDimension = width;
-      }
-      if (parseInt(size) == size) {
-        newDimension = parseInt(size);
-      }
-      image.resize(newDimension, newDimension);
-      image.getBase64Async(imageMime).then((buffer) => {
-        res.send({
-          width,
-          height,
-          newDimension,
-          data: buffer
+  if (req.isAuthenticated()) {
+    try {
+      const userName = req.user && req.user.userName;
+      const newDimension = parseInt(req.body.newSize);
+      const fileBuffer = req.file && req.file.buffer;
+      Jimp.read(fileBuffer).then(image => {
+        if (!helpers.checkImageValidity(image)) {
+          throw new Error('invalid file');
+        }
+        const height = image.getHeight();
+        const width = image.getWidth();
+        const imageMime = image.getMIME();
+        const imageClone = image.clone();
+        imageClone.write(`./uploads/${userName}/${Date.now()}.${image.getExtension()}`);
+        image.resize(newDimension, newDimension);
+        image.getBase64Async(imageMime).then((buffer) => {
+          res.send({
+            width,
+            height,
+            newDimension,
+            data: buffer
+          });
         });
       });
-    }).catch(err => {
+    } catch (e) {
       res.sendStatus(500);
-    });
-  });
+    }
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-  connectDb();
+  helpers.connectDb(mongoose);
 });
